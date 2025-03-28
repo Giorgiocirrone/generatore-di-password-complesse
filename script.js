c0a1ec83662063669fcc59c52e131dafd276f3af
Giorgiocirrone <cirronegiorgio@gmail.com>
2025-03-28 13:05:16 +0100

    Generatore password complesse

---
+++ script.js
@@ -0,0 +1,27 @@
+/**
+ * Generatore di password complesse
+ * creare un generatore di passowrd complesse
+ * 
+ */
+
+/**
+ * Step1 
+ * Prendere tutti gli elementi dal Dom
+ */
+
+/**
+ * Step 2 
+ * creare tutte le variabili 
+ */
+
+/**
+ * Step 3 impostare tutta la logica con un ciclo 
+ */
+
+
+/**
+ * Step 4 
+ * collegare all'evento del bottone
+ */
+
+
